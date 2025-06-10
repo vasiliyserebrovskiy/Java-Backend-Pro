ee3299de730e865db06cc79fe6094081964ccf4e
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-06-10 13:24:58 +0200

    lesson 3 added

---
+++ Lesson03/src/ait/elevator/ElevatorAppl.java
@@ -0,0 +1,8 @@
+package ait.elevator;
+
+/**
+ * @author Vasilii Serebrovskii
+ * @version 1.0 (10.06.2025)
+ */
+public class ElevatorAppl {
+}
---
+++ Lesson03/src/ait/timer/TimerAppl.java
@@ -0,0 +1,8 @@
+package ait.timer;
+
+/**
+ * @author Vasilii Serebrovskii
+ * @version 1.0 (10.06.2025)
+ */
+public class TimerAppl {
+}
